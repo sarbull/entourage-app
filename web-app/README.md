afe6fee68f6052c474302018aae9be377804da2f
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-01-28 22:27:31 +0200

    removed ui app

--- web-app/README.md
+++
@@ -1,6 +0,0 @@
-### Start App
-```bash
-$ npm install
-$ bower install
-$ grunt server # on port 8282 with live reload on every change
-```
--- web-app/js/app.js
+++
@@ -1,5 +0,0 @@
-var entourage = angular.module('entourage', []);
-
-entourage.controller('IndexController', function($scope) {
-  $scope.title = 'Hello world!';
-});
