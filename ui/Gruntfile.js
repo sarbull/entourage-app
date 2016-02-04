caf540ec2b0c08404cb4f469a302520a16f27df1
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-02-05 00:43:15 +0200

    Update gruntfile

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -2,10 +2,10 @@ module.exports = function(grunt) {
 
 
   var jsLibFiles = [
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/bootstrap/dist/js/bootstrap.min.js',
-    'bower_components/angular/angular.min.js',
-    'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
+    './bower_components/jquery/dist/jquery.min.js',
+    './bower_components/bootstrap/dist/js/bootstrap.min.js',
+    './bower_components/angular/angular.min.js',
+    './bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
   ];
 
   var jsAppFiles = [
